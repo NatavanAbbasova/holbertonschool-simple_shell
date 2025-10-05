77f5d01c70d4219a37459e1205b26ea2c1372688
root <11812@holbertonstudents.com>
2025-10-05 20:33:21 +0000

    kjgdkjfgfk

--- shell.h
+++ shell.h
@@ -1,16 +1,8 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <errno.h>
-
 void print_prompt(void);
-void execute_command(char *cmd);
+void execute_command(char *cmd, char **envp);
 
-#endif /* SHELL_H */
+#endif
 
