13bd2b97ef63471b6d877aa3156fb6ad1025b772
root <11812@holbertonstudents.com>
2025-10-05 19:40:02 +0000

    kjgdkjfgfk

--- prompt.c
+++ prompt.c
@@ -1,10 +1,13 @@
 #include "shell.h"
 
 /**
- * print_prompt - Displays the shell prompt
+ * print_prompt - Displays the shell prompt (only in interactive mode)
  */
 void print_prompt(void)
 {
-	printf("#cisfun$ ");
-	fflush(stdout);
+	if (isatty(STDIN_FILENO))
+	{
+		printf("#cisfun$ ");
+		fflush(stdout);
+	}
 }
